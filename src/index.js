120d697084bf08bef3dc8da5ccf8976eaa0acb19
linazoo <linazooper@gmail.com>
2019-01-21 16:23:37 -0600

    built a simple click counter demo with react

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let model = { clicks: 0 };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function render() {
+  ReactDOM.render(<App 
+                      clicks={model.clicks} 
+                      onCLick={()=> { model.clicks += 1; render(); }}/>, document.getElementById('root'));
+
+  }
+render();
